e1806945846b1015993920c19bebcffc5f430411
Tiago <tiago.americano.03@gmail.com>
2020-11-18 21:30:26 -0300

    Adicionei uma configuração de ssl no socket-client

--- frontend/src/socket.tsx
+++ frontend/src/socket.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, Children } from "react";
 
 import io from 'socket.io-client'
-const socket = io(process.env.REACT_APP_SOCKET_URL || '') 
+const socket = io(process.env.REACT_APP_SOCKET_URL || '', {secure: true}) 
 const SocketContext = createContext(socket);
 
 
