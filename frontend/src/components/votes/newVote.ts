965d06ac733b296806201c408fe05a72ed1f766f
Tysocope <rafamusa.20@gmail.com>
2021-05-26 18:32:50 -0300

    Mudei o nome do arquivo newVote.ts para styleNewVote.ts

--- frontend/src/components/votes/newVote.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-
-export default styled.div`
-  border-radius: 10px;
-  width: 500px;
-  height: 500px;
-  border: solid, 1px, black;
-`
---
+++ frontend/src/components/votes/styleNewVote.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+
+export default styled.div`
+  border-radius: 10px;
+  width: 500px;
+  height: 500px;
+  border: solid, 1px, black;
+`
